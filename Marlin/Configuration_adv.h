7f565aeca39ea21388a9e77287478f49794df6b0
Eugenijus <liutas4x4@gmail.com>
2017-12-23 22:44:30 +0200

    GT I3 X default params restore
    MK2a bed timing to 90 sec.
    Enable BABYSTEPPING for Z probe debugging.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -93,7 +93,7 @@
   /**
    * As described above, except for the bed (M140/M190/M303).
    */
-  #define WATCH_BED_TEMP_PERIOD 60                // Seconds
+  #define WATCH_BED_TEMP_PERIOD 90                // Seconds ** liutas4x4: On some ABS 60 it is too small amount
   #define WATCH_BED_TEMP_INCREASE 2               // Degrees Celsius
 #endif
 
@@ -655,7 +655,7 @@
  *
  * Warning: Does not respect endstops!
  */
-//#define BABYSTEPPING
+#define BABYSTEPPING
 #if ENABLED(BABYSTEPPING)
   //#define BABYSTEP_XY              // Also enable X/Y Babystepping. Not supported on DELTA!
   #define BABYSTEP_INVERT_Z false    // Change if Z babysteps should go the other way
